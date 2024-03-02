d7e384879bfc3157ba639cbeed8bd998a92d26f1
yaolifeng <y15829485647@163.com>
2024-03-02 23:36:55 +0800

    feat: v1.0

--- README.md
+++ README.md
@@ -10,4 +10,17 @@
 
 
 ### Step
-1.
+1.  Get Token: https://github.com/settings/tokens/new?scopes=delete_repo,repo&description=Repo%20Remover%20Token
+
+
+### Npm Package
+1.  octokit:
+    -   Github: https://github.com/octokit/octokit.js/#readme
+2.  @octokit/rest
+
+
+### TODO：
+1.  将所有仓库列为一个列表，让用户进行多选，对已选择进行删除
+2.  提示正在删除的第几个、或在终端中显示删除的进度
+3.  最后进行二次确认删除，把已选择的repo列出来
+4.  最后在删除
--- index.js
+++ index.js
@@ -1,22 +1,162 @@
 const { Octokit } = require('@octokit/rest');
+const inquirer = require('inquirer');
+const open = require('open');
+const os = require('os');
+const chalk = require('chalk');
+const fs = require('fs');
+const ora = require('ora');
+// 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
 
-// 创建一个 Octokit 实例
-const octokit = new Octokit({
-    auth: '你的 GitHub 令牌',
-});
+// const PLATFORM = os.platform();
+// if (platform === 'darwin') {
+//     console.log('User is using macOS.');
+// } else if (platform === 'win32') {
+//     console.log('User is using Windows.');
+// } else if (platform === 'linux') {
+//     console.log('User is using Linux.');
+// } else {
+//     console.log('User platform is ' + platform);
+// }
 
-// 要删除的仓库列表
-const reposToDelete = ['repo1', 'repo2', 'repo3'];
 
-// 对每个仓库执行删除操作
-reposToDelete.forEach(async repo => {
+// 对于Linux和Mac用户
+// open('http://google.com');
+// 而在Windows上
+// start http://www.google.com
+
+
+// const GITHUB_LINK = 'https://github.com/settings/tokens/new?scopes=delete_repo,repo&description=Repo Remover Token';
+// const GITEE_LINK = '';
+
+let spinner = null;
+let platform = '';
+let token = '';
+let owner = null;
+let octokit = null;
+inquirer
+    .prompt([
+        {
+            type: 'list',
+            name: 'platform',
+            message: 'Please select a platform?',
+            choices: [
+                {
+                    name: 'Github',
+                    value: 'Github',
+                    description: 'Github',
+                },
+                {
+                    name: 'Gitee',
+                    value: 'Gitee',
+                    description: 'Gitee',
+                },
+            ],
+        },
+        {
+            type: 'input',
+            name: 'token',
+            message: 'Please enter your token?\n',
+        }
+    ])
+    .then(async answers => {
+        console.log('answers ---->', answers);
+
+        platform = answers.platform;
+        token = answers.token;
+        octokit = createOctokitInstance(token);
+
+        if (platform === 'Github') {
+            spinner = ora('Loading Repository ...').start();
+            let allRepos = await getUserAllRepos();
+            // console.log('allRepos ---->', allRepos);
+            owner = allRepos[0].owner;
+            await showRepos(allRepos);
+        } else if (platform === 'Gitee') {
+            console.log(chalk.red('暂不支持Gitee'));
+        }
+    });
+
+const getUserAllRepos = async () => {
     try {
-        await octokit.repos.delete({
-            owner: '你的 GitHub 用户名',
-            repo,
+
+
+        const { data } = await octokit.repos.listForAuthenticatedUser();
+
+        let allRepos = data.map(item => {
+            return {
+                owner: item.owner.login,
+                name: item.name,
+                fullName: item.full_name,
+                description: item.description,
+                url: item.html_url,
+                language: item.language,
+                details: {
+                    stars: item.stargazers_count,
+                    forks: item.forks_count,
+                    watchers: item.watchers_count,
+                },
+                clone: {
+                    gitUrl: item.clone_url,
+                    sshUrl: item.ssh_url,
+                    svnUrl: item.svn_url,
+                }
+            }
         });
-        console.log(`成功删除仓库: ${repo}`);
-    } catch (err) {
-        console.error(`删除仓库失败: ${repo}`, err);
+        spinner.stop();
+        return allRepos;
+    } catch (error) {
+        console.error("Error fetching repos: ", error);
     }
-});
+}
+
+const showRepos = async (repos) => {
+    try {
+        let reposList = repos.map((item, index) => {
+            return {
+                name: item.fullName,
+                value: item.name,
+                description: item.description,
+            }
+        });;
+        const answers = await inquirer.prompt([
+            {
+                type: 'checkbox',
+                message: 'Please select repositories to delete.',
+                name: 'repos',
+                choices: reposList,
+                validate: function (answer) {
+                    if (answer.length < 1) {
+                        return 'You must choose at least one topping.';
+                    }
+                    return true;
+                }
+            }
+        ]);
+
+        console.log(answers);
+        await deleteRepos(octokit, answers.repos);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+async function deleteRepos(octokit, reposToDelete) {
+    for (const repo of reposToDelete) {
+        try {
+            await octokit.repos.delete({
+                owner: owner,
+                repo,
+            });
+            console.log(`成功删除仓库: ${repo}`);
+        } catch (err) {
+            console.error(`删除仓库失败: ${repo}`, err);
+        }
+    }
+}
+
+function createOctokitInstance(token) {
+    return new Octokit({
+        auth: token,
+    });
+}
