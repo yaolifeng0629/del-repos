7c745dfd63b6a9a1e6950556070f6ce5f330c8a0
yaolifeng <y15829485647@163.com>
2024-03-21 09:40:24 +0800

    feat: update path

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import { Octokit } from '@octokit/rest';
 import inquirer from 'inquirer';
 import LogGuide from './package/guide.js';
--- package/guide.js
+++ package/guide.js
@@ -2,7 +2,7 @@ import boxen from 'boxen';
 import { logMsg } from './utils.js';
 
 const guideData = boxen(
-    `\n Steps: \n 1: Generate token(In the page that just opened) \n 2: Open cmd input: del-util \n 3: Select platform (default github) \n 4: Enter token \n 5: Select the repos to delete (enter to confirm) \n 6: Wait for deletion to complete \n \n Tips: The token filled in is only for permission verification, and there is no violation. After using this tool, you can also manually delete the corresponding token. \n\n`,
+    `\n Steps: \n 1: Generate token(In the page that just opened) \n 2: Open cmd input: del-repos \n 3: Select platform (default github) \n 4: Enter token \n 5: Select the repos to delete (enter to confirm) \n 6: Wait for deletion to complete \n \n Tips: The token filled in is only for permission verification, and there is no violation. After using this tool, you can also manually delete the corresponding token. \n\n`,
     {
         width: 60,
         height: 15,
